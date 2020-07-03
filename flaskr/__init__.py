0c910e09848cc49dc2444b644a2960e6af5b5466
Rishav Ghosh Dastidar <50802440+gogolgd96@users.noreply.github.com>
2020-07-03 13:49:35 +0530

    Update patient logic written properly

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -103,6 +103,49 @@ def create_app(test_config=None):
                 print(patient_details)
                 return render_template('patient/delete_patient.html',patients=patient_details)
         return render_template('pharmacist/view_patient.html')
+    @app.route('/update_patient',methods=('GET', 'POST'))
+    @login_required
+    def update_patient():
+        if request.method=='POST': 
+            patient_id=request.form['patient']
+            if not patient_id:
+                error="First search the patient you want to delete"
+                flash(error)
+            else:
+                patient_id=request.form['patient']
+                patient_name = request.form['patient']
+                patient_age = request.form['patient']
+                date_of_admission = request.form['patient']
+                type_of_room = request.form['patient']
+                address = request.form['patient']
+                state = request.form['patient']
+                city= request.form['patient']
+                db3=db.get_db()
+                db3.execute('Update patient set patient_name=?,patient_age=?,date_of_admission=?,type_of_room=?,address=?,state=?,city=? where patient_id= ?',(patient_name,patient_age,date_of_admission,type_of_room,address,state,city,patient_id))
+                redirect(url_for('view_patients'))
+                db3.commit()
+                flash("Patient Updated Successfully")
+            return render_template('patient/update_patient.html')
+        return render_template('patient/update_patient.html')
+    
+    @app.route('/update_patient_id',methods=('GET', 'POST'),endpoint="get_patient2")
+    def get_patient2():
+        if request.method == 'POST':
+            patient_id = request.form['patient_id']
+            db1=db.get_db()
+            error= None
+
+            if not patient_id:
+                error="patient_SSN_id is required"
+                flash(error)
+            else:
+                patient_details=db1.execute(
+                    'SELECT * FROM patient WHERE patient_id = ?', (patient_id,)
+                    ).fetchall()
+                print(5)
+                print(patient_details)
+                return render_template('patient/update_patient.html',patients=patient_details)
+        return render_template('pharmacist/view_patient.html')
 
     db.init_app(app)
 
